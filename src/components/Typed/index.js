c909acf9164049ad6db09b907d46aed34f203050
anancodec <aafrida@wesleyan.edu>
2024-09-15 15:15:49 -0400

    remove react typed

--- src/components/Typed/index.js
+++ src/components/Typed/index.js
@@ -1,4 +1,3 @@
-import Typed from 'react-typed';
 import {TOP_SECTION} from '../../Module/General';
 import './style.css';
 
@@ -6,7 +5,7 @@ export default function MytypedComponent() {
   return (
     <div className="typing-div">
       <h1>
-        <Typed strings={TOP_SECTION.Typed_effect} typeSpeed={80} loop />
+        <div strings={TOP_SECTION.Typed_effect} typeSpeed={80} loop />
       </h1>
     </div>
   );
